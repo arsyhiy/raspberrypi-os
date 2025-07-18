83b3cecccdd9d68e40923ed1d4369f4f34f8faaf
Arseniy <arsyhiy32@gmail.com>
2025-07-18 13:04:18 +0300

    Update requiremnets.sh

--- requiremnets.sh
+++ requiremnets.sh
@@ -1,3 +1,4 @@
 sudo apt-get -y install gcc-arm-none-eabi     
 sudo apt install -y nasm 
-sudo apt install qemu-system-i386
+sudo apt install -y qemu-system-i386
+sudo apt install -y qemu-system-arm
